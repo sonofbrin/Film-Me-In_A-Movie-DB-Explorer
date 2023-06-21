52fb3ec852006fb87f2294383b04a15bc1133b21
Stevie <stevie.brinson@gmail.com>
2023-06-21 15:13:37 -0400

    Corrected attribution logo reference for deployment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
+import tmdbLogo from "../../public/tmdb-logo.svg"
+
 export default function Header() {
     return (
         <header>
             <h1>Film Me In!</h1>
             <section id="tmdb-attrib">
                 <span>A movie explorer powered by </span>
-                <img src="../../public/tmdb-logo.svg" alt="TMDB Logo"></img>
+                <img src={tmdbLogo} alt="TMDB Logo"></img>
             </section>
         </header>
     )
